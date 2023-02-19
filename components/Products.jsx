3299a03a681b35da7bdc2c1683f5546223106ff0
thilaks <thilak7c@gmail.com>
2023-02-19 21:56:38 +0800

    First commit

--- components/Products.jsx
+++ components/Products.jsx
@@ -75,7 +75,7 @@ const Products = () => {
 
   return (
     <div className='w-full bg-[#F8F8F8] py-32'>
-        <div className='md:max-w-[1024px] m-auto max-w-[600px]'>
+        <div className='md:max-w-[1024px] m-auto max-w-[600px] px-4 md:px-0'>
             <div className='py-4'>
               <h1 className='py-3 text-3xl font-bold'>Best Selling</h1>
               <p className='text-[#6D737A]'>Enjoy Up To 50%</p>
