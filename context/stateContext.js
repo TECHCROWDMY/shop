b017c97f9c95866573451671b589bbed3b216f7b
thilaks <thilak7c@gmail.com>
2023-02-26 17:13:34 +0800

    Third commit

--- context/stateContext.js
+++ context/stateContext.js
@@ -3,6 +3,7 @@ import { toast } from 'react-hot-toast';
 
 const Context = createContext();
 
+
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
