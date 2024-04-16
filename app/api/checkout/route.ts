0c6c5dc96da81ff2f1d0f27f8e0488aff854bafa
Thilak A/L Sundaram <thilak014@gmail.com>
2024-04-17 02:27:42 +0800

    Fixed url endpoint

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -56,8 +56,8 @@ export const POST = async (request:any) => {
      const session = await stripe.checkout.sessions.create({
         line_items:stripeProducts,
         mode: 'payment',
-        success_url: `http://localhost:3000/success`,
-        cancel_url: `http://localhost:3000/`,
+        success_url: 'https://shop-six-ecru.vercel.app/success',
+        cancel_url: 'https://shop-six-ecru.vercel.app/',
       });
       
     return NextResponse.json({
